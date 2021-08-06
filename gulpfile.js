d961f3cb120a5c095c8f8f34366c09140298b5c8
Kostunin Mike <kostuninmike@MacBook-Pro-Kostunin-2.local>
2021-08-06 20:19:30 +0300

    test-style

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp          = require('gulp');
 const themeKit = require('@shopify/themekit')
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task( 'sass', function()  {
     return gulp.src('scss/global.scss')
       .pipe(sass())
+      .pipe(cleanCSS({compatibility:'ie11'}))
       .pipe(gulp.dest('assets'))
   });
 
@@ -14,3 +16,5 @@ gulp.task( 'sass', function()  {
         env: 'development'
     })  
   });
+
+  gulp.task('default');
